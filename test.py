5048eb4f65f498542e6ffc9a19875fe4a987ab59
jonathan Wong <jonathanwong0716@gmail.com>
2024-05-08 19:26:26 +1200

    Fixed bug re print vs input

--- test.py
+++ test.py
@@ -1,2 +1,20 @@
-print("hello 2")
+import random
 
+
+print("Welcome to the Guessing Game!")
+print("Try to guess the number I am thinking of between 1 and 200.")
+
+number = random.randint(1,200)  # generate random number
+guess = -1  # initialize guess to an invalid value
+
+while guess != number:
+    guess_str = input("Enter your number: ")
+    guess = int(guess_str)
+
+    if guess < number:
+        print("Too low, try again.")
+    elif guess > number:
+        print("Too high, try again.")
+
+print("Congratulations! You guessed the number {number} correctly!") 
+input("game over")
